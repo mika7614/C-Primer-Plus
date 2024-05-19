67702d795f1bdfc7cbbf0c49c1e4611030385112
mika7614 <68652647+mika7614@users.noreply.github.com>
2024-05-19 22:25:57 +0800

    Add files via upload

---
+++ 第3章/section3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<inttypes.h>
+int main(void)
+{
+/*//char beep='\07';
+printf("\07");
+return 0;
+}
+	*/
+	int16_ me16;
+	
+	me16=4593;
+	printf("me16=%hd\n",me16);
+	printf(Instead,use a \*macro\* from inttypes.h:");
+		printf("me16=%"PRId16"\n",me16);
+	return 0;
+}
+//该编译器不存在头文件inttypes.h
